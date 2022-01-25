34a4498a8b638fcfbfec999ce364ad871c4b71d5
alepilnik <alejopilnik@hotmail.com>
2022-01-25 00:21:03 -0300

    Initial commit

---
+++ src/com/tiposdatos/Datos.java
@@ -0,0 +1,49 @@
+package com.tiposdatos;
+
+public class Datos {
+
+    public static void main(String[] args) {
+
+    // 1. Numericos
+
+    // 1.a Enteros
+
+    byte varbyte = 9;
+    System.out.println(varbyte);
+
+    short varshort = 3;
+    System.out.println(varshort);
+
+    int varint = 10;
+    System.out.println(varint);
+
+    long varlong = 789542103;
+    System.out.println(varlong);
+
+    // 1.b Decimales
+
+    float varfloat = 4.9f;
+    System.out.println(varfloat);
+
+    double vardouble = 9.99d;
+    System.out.println(vardouble);
+
+    // 2. Booleanos
+
+    boolean varbooleant = true;
+    System.out.println(varbooleant);
+
+    boolean varbooleanf = false;
+    System.out.println(varbooleanf);
+
+    // 3. Texto
+
+    String varstring = "Alejo";
+    System.out.println(varstring);
+
+    char varchaar = 'a';
+    System.out.println(varchaar);
+
+    }
+
+}
